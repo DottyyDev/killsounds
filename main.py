329013cbc7e42bfd6148132aa9b6c69b9fa2408c
DottyyDev <91227484+DottyyDev@users.noreply.github.com>
2022-08-10 16:22:10 -0400

    got rid of god and hoppper list, made sounds for kills the valorant kill sounds

--- main.py
+++ main.py
@@ -1,37 +1,32 @@
+
 from playsound import playsound
 
 txt = open('./values.txt')
 print('discovered values.txt')
 txt = txt.readline()
 
+kills = 0
+
 thefile = open(txt)
 print('found the specified logs files')
 
-listen = ['+1 Ender Pearl! (Black Magic Perk)','You have assisted killing','You won! Want to play again? Click here! ','+1 SkyWars Experience (Kill)'] # What the program should look for in the chat logs
+listen = ['+1 Ender Pearl!','You have assisted killing','SkyWars Experience (Win)','SkyWars Experience (Kill)'] # What the program should look for in the chat logs
 
 play = ['pearl','assist','win','kill'] # The corresponding sound's name, in the same position on the list
 
-hoppers = []
-gods = []
+
 thefile.seek(0, 2)
 print('entering the loop!')
 while True:
-
     line = thefile.readline()
     for x in listen:
         if x in line:
-            playsound(str('./assets/'+play[listen.index(x)]+'.mp3'))
-            break    
-    for x in hoppers:
-        if x in line:
-            playsound('./assets/hecker.mp3')
-            break
-    for x in gods:
-        if x in line:
-            playsound('./assets/god.mp3')
-    if '!hopper ' in line:
-        hoppers.append(line.split('!hopper ',1)[1])
-        print('added to hopper list!')
-    if '!god' in line:
-        gods.append(line.split('god ',1)[1])
-        print('added to god list!')
+            if x == 'SkyWars Experience (Kill)':
+                if kills < 5:
+                    kills += 1
+                playsound(str('./assets/kill/'+str(kills)+'.mp3'), False)
+            else:
+                playsound(str('./assets/'+play[listen.index(x)]+'.mp3'), False)
+                break    
+    if 'Cages opened! FIGHT!' in line:
+        kills = 0
